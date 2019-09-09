8a0de3f6dab28e3c2927f0532579b779dc62ed09
Natalie Soberano <nataliesoberano@Natalies-MBP-4.fc.ca>
2019-09-09 19:46:49 -0400

    addJsfiles

---
+++ js/modules/DataObject.js
@@ -0,0 +1,12 @@
+// objects are just collections of keys and valies
+// remeber to seperate the keys and values with a comma 
+// and use a colon between the key and value, never and equals sign
+
+const Person = {
+    name: "Natalie",
+    eyeColour: "green",
+    hairColour: "brown",
+    role: "student"
+}
+
+export default Person;
